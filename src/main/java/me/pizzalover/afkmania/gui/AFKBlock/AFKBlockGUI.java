2fb93250893130295a5ed9ccc238a771cc8908bb
PizzaThatCodes <pizzathatcodes@gmail.com>
2026-02-22 00:31:08 -0500

    Finish AFK block module with rewards and upgrade system

--- src/main/java/me/pizzalover/afkmania/gui/AFKBlock/AFKBlockGUI.java
+++ src/main/java/me/pizzalover/afkmania/gui/AFKBlock/AFKBlockGUI.java
@@ -3,6 +3,7 @@ package me.pizzalover.afkmania.gui.AFKBlock;
 import me.pizzalover.afkmania.Main;
 import me.pizzalover.afkmania.gui.guiUtils;
 import me.pizzalover.afkmania.utils.utils;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Player;
 import xyz.xenondevs.invui.gui.Gui;
 import xyz.xenondevs.invui.window.Window;
@@ -14,8 +15,8 @@ public class AFKBlockGUI {
         String configPath = "gui";
         int slotAmount = Main.getAfkBlockConfig().getConfig().getInt(configPath + ".slot_amount");
 
-        if(slotAmount % 9 != 0) {
-            player.sendMessage(utils.translate( utils.addPlaceholderToText(player, Main.getMessageConfig().getConfig().getString("afk_block.gui.error_messages.slot_amount_not_divisible_by_9")
+        if (slotAmount % 9 != 0) {
+            player.sendMessage(utils.translate(utils.addPlaceholderToText(player, Main.getMessageConfig().getConfig().getString("afk_block.gui.error_messages.slot_amount_not_divisible_by_9")
                     .replace("%prefix%", Main.getSettingConfig().getConfig().getString("prefix"))
                     .replace("%gui%", Main.getAfkBlockConfig().getConfig().getString(configPath + ".gui_name"))
                     .replace("%slot_number%", String.valueOf(slotAmount))
@@ -23,16 +24,16 @@ public class AFKBlockGUI {
             return;
         }
 
-        if(slotAmount > 54) {
-            player.sendMessage(utils.translate( utils.addPlaceholderToText(player, Main.getMessageConfig().getConfig().getString("afk_block.gui.error_messages.slot_amount_greater_than_54")
+        if (slotAmount > 54) {
+            player.sendMessage(utils.translate(utils.addPlaceholderToText(player, Main.getMessageConfig().getConfig().getString("afk_block.gui.error_messages.slot_amount_greater_than_54")
                     .replace("%prefix%", Main.getSettingConfig().getConfig().getString("prefix"))
                     .replace("%gui%", Main.getAfkBlockConfig().getConfig().getString(configPath + ".gui_name"))
                     .replace("%slot_number%", String.valueOf(slotAmount))
             )));
             return;
         }
-        if(slotAmount < 9) {
-            player.sendMessage(utils.translate( utils.addPlaceholderToText(player, Main.getMessageConfig().getConfig().getString("afk_block.gui.error_messages.slot_amount_less_than_9")
+        if (slotAmount < 9) {
+            player.sendMessage(utils.translate(utils.addPlaceholderToText(player, Main.getMessageConfig().getConfig().getString("afk_block.gui.error_messages.slot_amount_less_than_9")
                     .replace("%prefix%", Main.getSettingConfig().getConfig().getString("prefix"))
                     .replace("%gui%", Main.getAfkBlockConfig().getConfig().getString(configPath + ".gui_name"))
                     .replace("%slot_number%", String.valueOf(slotAmount))
@@ -44,31 +45,46 @@ public class AFKBlockGUI {
         Gui gui = Gui.empty(9, height);
 
 
+        ConfigurationSection slotsSection = Main.getAfkBlockConfig().getConfig().getConfigurationSection(configPath + ".slots");
+        if (slotsSection != null) {
+            for (String slot : slotsSection.getKeys(false)) {
+                String path = configPath + ".slots." + slot + ".";
 
-        for(String slot : Main.getAfkBlockConfig().getConfig().getConfigurationSection(configPath + ".slots").getKeys(false)) {
-            String path = configPath + ".slots." + slot + ".";
+                int slotNum;
 
-            int slotNum;
+                try {
+                    slotNum = Integer.parseInt(slot);
+                } catch (NumberFormatException exception) {
+                    player.sendMessage(utils.translate(utils.addPlaceholderToText(player, Main.getMessageConfig().getConfig().getString("afk_block.gui.error_messages.invalid_slot_number")
+                            .replace("%prefix%", Main.getSettingConfig().getConfig().getString("prefix"))
+                            .replace("%gui%", Main.getAfkBlockConfig().getConfig().getString(configPath + ".gui_name"))
+                            .replace("%slot_number%", slot)
+                    )));
+                    continue;
+                }
 
-            try {
-                slotNum = Integer.parseInt(slot);
-            } catch (NumberFormatException exception) {
-                player.sendMessage(utils.translate( utils.addPlaceholderToText(player, Main.getMessageConfig().getConfig().getString("afk_block.gui.error_messages.invalid_slot_number")
-                        .replace("%prefix%", Main.getSettingConfig().getConfig().getString("prefix"))
-                        .replace("%gui%", Main.getAfkBlockConfig().getConfig().getString(configPath + ".gui_name"))
-                        .replace("%slot_number%", slot)
-                )));
-                continue;
+                gui.setItem(slotNum, guiUtils.createInventoryItem(player, path));
+
             }
+        }
 
-            gui.setItem(slotNum, guiUtils.createInventoryItem(player, path));
+        ConfigurationSection upgrades = Main.getAfkBlockConfig().getConfig().getConfigurationSection("upgrades");
+        if (upgrades != null) {
+            for (String key : upgrades.getKeys(false)) {
+                String path = "upgrades." + key + ".";
+                if (!Main.getAfkBlockConfig().getConfig().getBoolean(path + "enabled", false)) continue;
 
+                int slot = Main.getAfkBlockConfig().getConfig().getInt(path + "slot", -1);
+                if (slot < 0 || slot >= slotAmount) continue;
+
+                gui.setItem(slot, guiUtils.createUpgradeItem(player, key));
+            }
         }
 
         Window window = Window.single()
                 .setViewer(player)
                 .setGui(gui)
-                .setTitle(utils.translate( utils.addPlaceholderToText(player, Main.getAfkBlockConfig().getConfig().getString(configPath + ".gui_name"))))
+                .setTitle(utils.translate(utils.addPlaceholderToText(player, Main.getAfkBlockConfig().getConfig().getString(configPath + ".gui_name"))))
                 .build();
         window.open();
 
--- src/main/java/me/pizzalover/afkmania/gui/guiUtils.java
+++ src/main/java/me/pizzalover/afkmania/gui/guiUtils.java
@@ -2,6 +2,8 @@ package me.pizzalover.afkmania.gui;
 
 import com.cryptomorin.xseries.XMaterial;
 import me.pizzalover.afkmania.Main;
+import me.pizzalover.afkmania.modules.AFKBlockModules;
+import me.pizzalover.afkmania.player_info.afk_block.AFKBlockPlayerData;
 import me.pizzalover.afkmania.utils.SkullCreator;
 import me.pizzalover.afkmania.utils.utils;
 import org.bukkit.Bukkit;
@@ -21,48 +23,43 @@ import java.util.ArrayList;
 
 public class guiUtils {
 
-    /**
-     * Create an inventory item
-     * @param player The player
-     * @param path The path to the item in the config
-     * @return The item
-     */
     public static AbstractItem createInventoryItem(Player player, String path) {
         return new AbstractItem() {
             @Override
             public ItemProvider getItemProvider() {
-                if(Main.getAfkBlockConfig().getConfig().getString(path + "item").split("-")[0].equalsIgnoreCase("base64")) {
+                if (Main.getAfkBlockConfig().getConfig().getString(path + "item").split("-")[0].equalsIgnoreCase("base64")) {
                     String base64String = Main.getAfkBlockConfig().getConfig().getString(path + "item").replace("base64-", "");
-                    ItemStack base64Skull = SkullCreator.itemWithBase64(XMaterial.PLAYER_HEAD.parseItem(), base64String );
+                    ItemStack base64Skull = SkullCreator.itemWithBase64(XMaterial.PLAYER_HEAD.parseItem(), base64String);
                     SkullMeta base64SkullMeta = (SkullMeta) base64Skull.getItemMeta();
 
-                    base64SkullMeta.setDisplayName(utils.translate( utils.addPlaceholderToText(player, Main.getAfkBlockConfig().getConfig().getString(path + "name"))));
+                    base64SkullMeta.setDisplayName(utils.translate(utils.addPlaceholderToText(player, Main.getAfkBlockConfig().getConfig().getString(path + "name"))));
 
-                    ArrayList<String> lore = new ArrayList<>();
+                    ArrayList<String> lore = new ArrayList<String>();
 
-                    for(String s : Main.getAfkBlockConfig().getConfig().getStringList(path + "lore")) {
-                        for(String str : s.split("<br>"))
-                            lore.add(utils.translate( utils.addPlaceholderToText(player, str)));
+                    for (String s : Main.getAfkBlockConfig().getConfig().getStringList(path + "lore")) {
+                        for (String str : s.split("<br>"))
+                            lore.add(utils.translate(utils.addPlaceholderToText(player, str)));
                     }
                     base64SkullMeta.setLore(lore);
                     base64Skull.setItemMeta(base64SkullMeta);
 
                     return new ItemBuilder(base64Skull);
-                }
-                else if(Main.getAfkBlockConfig().getConfig().getString(path + "item").split("-")[0].equalsIgnoreCase("head")) {
+                } else if (Main.getAfkBlockConfig().getConfig().getString(path + "item").split("-")[0].equalsIgnoreCase("head")) {
                     ItemStack item = new ItemStack(Material.PLAYER_HEAD, 1);
                     SkullMeta pSkull = (SkullMeta) item.getItemMeta();
-                    pSkull.setDisplayName(utils.translate( utils.addPlaceholderToText(player, Main.getAfkBlockConfig().getConfig().getString(path + "name"))));
+                    pSkull.setDisplayName(utils.translate(utils.addPlaceholderToText(player, Main.getAfkBlockConfig().getConfig().getString(path + "name"))));
 
-                    ArrayList<String> lore = new ArrayList<>();
+                    ArrayList<String> lore = new ArrayList<String>();
 
-                    for(String s : Main.getAfkBlockConfig().getConfig().getStringList(path + "lore")) {
-                        for(String str : s.split("<br>"))
-                            lore.add(utils.translate( utils.addPlaceholderToText(player, str)));
+                    for (String s : Main.getAfkBlockConfig().getConfig().getStringList(path + "lore")) {
+                        for (String str : s.split("<br>"))
+                            lore.add(utils.translate(utils.addPlaceholderToText(player, str)));
                     }
                     pSkull.setLore(lore);
                     String playerName = Main.getAfkBlockConfig().getConfig().getString(path + "item").split("-")[1];
-                    if(playerName.contains("{")) {playerName = player.getName();}
+                    if (playerName.contains("{")) {
+                        playerName = player.getName();
+                    }
                     pSkull.setOwner(playerName);
                     item.setItemMeta(pSkull);
                     return new ItemBuilder(item);
@@ -70,12 +67,12 @@ public class guiUtils {
                 ItemStack itemStack = new ItemStack(XMaterial.valueOf(Main.getAfkBlockConfig().getConfig().getString(path + "item")).parseMaterial(), 1);
                 ItemMeta itemMeta = itemStack.getItemMeta();
 
-                itemMeta.setDisplayName(utils.translate( utils.addPlaceholderToText(player, Main.getAfkBlockConfig().getConfig().getString(path + "name"))));
+                itemMeta.setDisplayName(utils.translate(utils.addPlaceholderToText(player, Main.getAfkBlockConfig().getConfig().getString(path + "name"))));
 
-                ArrayList<String> lore = new ArrayList<>();
+                ArrayList<String> lore = new ArrayList<String>();
 
-                for(String s : Main.getAfkBlockConfig().getConfig().getStringList(path + "lore")) {
-                    for(String str : s.split("<br>"))
+                for (String s : Main.getAfkBlockConfig().getConfig().getStringList(path + "lore")) {
+                    for (String str : s.split("<br>"))
                         lore.add(utils.translate(utils.addPlaceholderToText(player, str)));
                 }
                 itemMeta.setLore(lore);
@@ -87,15 +84,15 @@ public class guiUtils {
             @Override
             public void handleClick(ClickType clickType, Player player, InventoryClickEvent inventoryClickEvent) {
 
-                if(clickType.isLeftClick()) {
-                    for(String s : Main.getAfkBlockConfig().getConfig().getStringList(path + "left_click_commands")) {
-                        if(s.startsWith("[player]")) {
+                if (clickType.isLeftClick()) {
+                    for (String s : Main.getAfkBlockConfig().getConfig().getStringList(path + "left_click_commands")) {
+                        if (s.startsWith("[player]")) {
                             player.performCommand(utils.addPlaceholderToText(player, s).replace("[player] ", ""));
                         } else if (s.startsWith("[console]")) {
                             utils.runConsoleCommand(utils.addPlaceholderToText(player, s).replace("[console] ", ""), player.getWorld());
                         } else if (s.startsWith("[sound]")) {
                             String[] soundVariables = s.replace("[sound] ", "").split(" ");
-                            player.playSound(player, Sound.valueOf(utils.addPlaceholderToText(player, soundVariables[0]) ), Integer.parseInt(soundVariables[1]), Integer.parseInt(soundVariables[2]));
+                            player.playSound(player, Sound.valueOf(utils.addPlaceholderToText(player, soundVariables[0])), Integer.parseInt(soundVariables[1]), Integer.parseInt(soundVariables[2]));
                         } else {
                             if (s.equalsIgnoreCase("exitInventory")) {
                                 player.closeInventory();
@@ -103,15 +100,15 @@ public class guiUtils {
                         }
                     }
                 }
-                if(clickType.isRightClick()) {
-                    for(String s : Main.getAfkBlockConfig().getConfig().getStringList(path + "right_click_commands")) {
-                        if(s.startsWith("[player]")) {
+                if (clickType.isRightClick()) {
+                    for (String s : Main.getAfkBlockConfig().getConfig().getStringList(path + "right_click_commands")) {
+                        if (s.startsWith("[player]")) {
                             player.performCommand(utils.addPlaceholderToText(player, s).replace("[player] ", ""));
                         } else if (s.startsWith("[console]")) {
                             Bukkit.dispatchCommand(Bukkit.getConsoleSender(), utils.addPlaceholderToText(player, s).replace("[console] ", ""));
                         } else if (s.startsWith("[sound]")) {
                             String[] soundVariables = s.replace("[sound] ", "").split(" ");
-                            player.playSound(player, Sound.valueOf(utils.addPlaceholderToText(player, soundVariables[0]) ), Integer.parseInt(soundVariables[1]), Integer.parseInt(soundVariables[2]));
+                            player.playSound(player, Sound.valueOf(utils.addPlaceholderToText(player, soundVariables[0])), Integer.parseInt(soundVariables[1]), Integer.parseInt(soundVariables[2]));
                         } else {
                             if (s.equalsIgnoreCase("exitInventory")) {
                                 player.closeInventory();
@@ -122,6 +119,97 @@ public class guiUtils {
 
             }
         };
+    }
+
+    public static AbstractItem createUpgradeItem(Player player, String upgradeKey) {
+        return new AbstractItem() {
+            @Override
+            public ItemProvider getItemProvider() {
+                AFKBlockModules module = (AFKBlockModules) Main.getModuleManager().getModule("AFKBlock");
+                AFKBlockPlayerData data = module.getOrCreatePlayerData(player);
+
+                String path = "upgrades." + upgradeKey + ".";
+                Material material = XMaterial.valueOf(Main.getAfkBlockConfig().getConfig().getString(path + "item", "STONE")).parseMaterial();
+                ItemStack item = new ItemStack(material == null ? Material.STONE : material, 1);
+                ItemMeta meta = item.getItemMeta();
+
+                int level = data.getUpgradeLevel(upgradeKey);
+                int maxLevel = Main.getAfkBlockConfig().getConfig().getInt(path + "max_level", 10);
+                long cost = getUpgradeCost(path, level);
+
+                String name = Main.getAfkBlockConfig().getConfig().getString(path + "name", "&e" + upgradeKey);
+                meta.setDisplayName(utils.translate(name
+                        .replace("%upgrade_level%", String.valueOf(level))
+                        .replace("%next_level%", String.valueOf(level + 1))
+                ));
+
+                ArrayList<String> lore = new ArrayList<String>();
+                for (String line : Main.getAfkBlockConfig().getConfig().getStringList(path + "lore")) {
+                    lore.add(utils.translate(utils.addPlaceholderToText(player, line)
+                            .replace("%upgrade_level%", String.valueOf(level))
+                            .replace("%next_level%", String.valueOf(level + 1))
+                            .replace("%upgrade_cost%", String.valueOf(cost))
+                            .replace("%upgrade_max_level%", String.valueOf(maxLevel))
+                    ));
+                }
+                meta.setLore(lore);
+                item.setItemMeta(meta);
+                return new ItemBuilder(item);
+            }
+
+            @Override
+            public void handleClick(ClickType clickType, Player player, InventoryClickEvent event) {
+                if (!clickType.isLeftClick()) return;
+
+                AFKBlockModules module = (AFKBlockModules) Main.getModuleManager().getModule("AFKBlock");
+                AFKBlockPlayerData data = module.getOrCreatePlayerData(player);
+                String path = "upgrades." + upgradeKey + ".";
+                int level = data.getUpgradeLevel(upgradeKey);
+                int maxLevel = Main.getAfkBlockConfig().getConfig().getInt(path + "max_level", 10);
+
+                if (level >= maxLevel) {
+                    for (String command : Main.getAfkBlockConfig().getConfig().getStringList(path + "maxed_commands")) {
+                        utils.runConsoleCommand(formatUpgradeCommand(command, player, upgradeKey, level, 0), player.getWorld());
+                    }
+                    return;
+                }
+
+                long cost = getUpgradeCost(path, level);
+                for (String command : Main.getAfkBlockConfig().getConfig().getStringList(path + "purchase_commands")) {
+                    utils.runConsoleCommand(formatUpgradeCommand(command, player, upgradeKey, level, cost), player.getWorld());
+                }
+
+                data.setUpgradeLevel(upgradeKey, level + 1);
+                module.saveUpgrades(data);
+
+                for (String command : Main.getAfkBlockConfig().getConfig().getStringList(path + "after_purchase_commands")) {
+                    utils.runConsoleCommand(formatUpgradeCommand(command, player, upgradeKey, level + 1, cost), player.getWorld());
+                }
+
+                event.getWhoClicked().closeInventory();
+                Bukkit.getScheduler().runTaskLater(Main.getInstance(), new Runnable() {
+                    @Override
+                    public void run() {
+                        me.pizzalover.afkmania.gui.AFKBlock.AFKBlockGUI.openUpgradeGUI(player);
+                    }
+                }, 1L);
+            }
+        };
+    }
+
+    private static String formatUpgradeCommand(String command, Player player, String upgradeKey, int currentLevel, long cost) {
+        return utils.addPlaceholderToText(player, command)
+                .replace("%player%", player.getName())
+                .replace("%upgrade%", upgradeKey)
+                .replace("%upgrade_level%", String.valueOf(currentLevel))
+                .replace("%next_upgrade_level%", String.valueOf(currentLevel + 1))
+                .replace("%upgrade_cost%", String.valueOf(cost));
+    }
+
+    private static long getUpgradeCost(String path, int currentLevel) {
+        long baseCost = Main.getAfkBlockConfig().getConfig().getLong(path + "base_cost", 0);
+        long incrementalCost = Main.getAfkBlockConfig().getConfig().getLong(path + "incremental_cost", 0);
+        return Math.max(0, baseCost + (incrementalCost * currentLevel));
     }
 
 }
